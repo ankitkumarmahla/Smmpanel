0e6a971b426414b97cea3a7190aa1b2a200fc983
ankitkumarmahla <ankitkm734060@gmail.com>
2025-02-15 08:39:03 +0530

    Update app.js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyDbh__UitSrtQCcgnxcPMz31plcClC3ND4",
   authDomain: "panelsmm-10a25.firebaseapp.com",
   projectId: "panelsmm-10a25",
-  storageBucket: "panelsmm-10a25.firebasestorage.app",
+  storageBucket: "panelsmm-10a25.firebasestorage.com",
   messagingSenderId: "997413113102",
   appId: "1:997413113102:web:44ba6b4c7bc39708339b5f",
   measurementId: "G-0DWF5XZ108"
